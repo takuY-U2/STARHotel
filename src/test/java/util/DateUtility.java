c2481fa69ac3f416e2b6972857e2e0d7cd80ba24
kenichiro.ota <kenichiro.ota@shiftinc.jp>
2014-04-25 10:08:16 +0900

    add DateUtility

---
+++ src/test/java/util/DateUtility.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.util.Calendar;
+
+/**
+ * Created by kenichiro_ota on 2014/04/24.
+ */
+public class DateUtility {
+    public static Calendar getImmediateDayOfWeek(Calendar current, int dayOfWeek) {
+        Calendar immediateDayOfWeek = (Calendar) current.clone();
+        immediateDayOfWeek.add(Calendar.DAY_OF_MONTH, 1);
+
+        while (immediateDayOfWeek.get(Calendar.DAY_OF_WEEK) != dayOfWeek) {
+            immediateDayOfWeek.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        return immediateDayOfWeek;
+    }
+}
---
+++ src/test/java/util/DateUtilityTest.java
@@ -0,0 +1,65 @@
+package util;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by kenichiro_ota on 2014/04/24.
+ */
+public class DateUtilityTest {
+    @Test
+    public void testGetImmediateSaturdayOfWeekWhenTodayIsFriday() throws Exception {
+        Calendar friday = Calendar.getInstance();
+        friday.clear();
+        friday.set(2014, Calendar.APRIL, 25);
+
+        Calendar saturday = Calendar.getInstance();
+        saturday.clear();
+        saturday.set(2014, Calendar.APRIL, 26);
+
+        assertThat(DateUtility.getImmediateDayOfWeek(friday, Calendar.SATURDAY), is(saturday));
+    }
+
+    @Test
+    public void testGetImmediateSaturdayOfWeekWhenTodayIsSaturday() throws Exception {
+        Calendar saturday = Calendar.getInstance();
+        saturday.clear();
+        saturday.set(2014, Calendar.APRIL, 26);
+
+        Calendar nextSaturday = Calendar.getInstance();
+        nextSaturday.clear();
+        nextSaturday.set(2014, Calendar.MAY, 3);
+
+        assertThat(DateUtility.getImmediateDayOfWeek(saturday, Calendar.SATURDAY), is(nextSaturday));
+    }
+    
+    @Test
+    public void testGetImmediateSaturdayOfWeekWhenTodayIsSunday() throws Exception {
+        Calendar sunday = Calendar.getInstance();
+        sunday.clear();
+        sunday.set(2014, Calendar.APRIL, 27);
+
+        Calendar saturday = Calendar.getInstance();
+        saturday.clear();
+        saturday.set(2014, Calendar.MAY, 3);
+
+        assertThat(DateUtility.getImmediateDayOfWeek(sunday, Calendar.SATURDAY), is(saturday));
+    }
+
+    @Test
+    public void testGetImmediateSundayOfWeekWhenTodayIsSunday() throws Exception {
+        Calendar sunday = Calendar.getInstance();
+        sunday.clear();
+        sunday.set(2014, Calendar.APRIL, 27);
+
+        Calendar nextSaturday = Calendar.getInstance();
+        nextSaturday.clear();
+        nextSaturday.set(2014, Calendar.MAY, 4);
+
+        assertThat(DateUtility.getImmediateDayOfWeek(sunday, Calendar.SUNDAY), is(nextSaturday));
+    }
+}
