b16b71fa46be8e1286ec946502debe6f51880ef9
kenichiro.ota <kenichiro.ota@shiftinc.jp>
2014-05-12 21:08:56 +0900

    fix util public

--- src/test/java/util/CalendarUtility.java
+++ src/test/java/util/CalendarUtility.java
@@ -5,8 +5,8 @@ import java.util.Calendar;
 /**
  * Created by kenichiro_ota on 2014/04/24.
  */
-class CalendarUtility {
-    static Calendar getImmediateDayOfWeek(Calendar current, int dayOfWeek) {
+public class CalendarUtility {
+    public static Calendar getImmediateDayOfWeek(Calendar current, int dayOfWeek) {
         Calendar immediateDayOfWeek = (Calendar) current.clone();
         immediateDayOfWeek.add(Calendar.DAY_OF_MONTH, 1);
 
